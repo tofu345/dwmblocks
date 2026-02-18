c0bdd0a1914d613282839e6c869a8d5bef3b70cc
tofu345 <shammahayotunde1@gmail.com>
2026-02-18 20:52:42 +0000

    upd

--- blocks.def.h
+++ blocks.def.h
@@ -2,23 +2,23 @@
 
 #define SCRIPT(s) "~/dotfiles/bin/" s
 
-static char volume_cmd[] = SCRIPT("dwmb_volume.sh");
-static char wifi_cmd[] = SCRIPT("dwmb_wifi.sh");
+static char volume_cmd[]	= SCRIPT("dwmb_volume.sh");
+static char wifi_cmd[]		= SCRIPT("dwmb_wifi.sh");
 static char cpu_usage_cmd[] = SCRIPT("dwmb_cpu_usage.sh");
-static char cpu_load_cmd[] = SCRIPT("dwmb_cpu_load.sh");
-static char memory_cmd[] = SCRIPT("dwmb_memory.sh");
-static char battery_cmd[] = SCRIPT("dwmb_battery.sh");
-static char date_cmd[] = "date '+%a %b %d %H:%M '";
+static char cpu_load_cmd[]	= SCRIPT("dwmb_cpu_load.sh");
+static char memory_cmd[]	= SCRIPT("dwmb_memory.sh");
+static char battery_cmd[]	= SCRIPT("dwmb_battery.sh");
+static char date_cmd[]		= "date '+%a %b %d %H:%M '";
 
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /* Icon */  /* Command */	/* Update Interval */	/* Update Signal */
+    /* Icon		Command			Update (s)		Update Signal */
     { "",       volume_cmd,     1,		        1 },
     { "",       wifi_cmd,       1,		        0 },
     { "",       cpu_usage_cmd,  1,		        0 },
     { "",       cpu_load_cmd,   1,		        0 },
     { "",       memory_cmd,     1, 		        0 },
-    { "",       battery_cmd,    3,		        0 },
+    { "",       battery_cmd,    60,		        2 },
     { "",       date_cmd,       5,		        0 },
 };
 
