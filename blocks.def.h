09c057272da3ce3ba8801f6cd5e5bb1118e58f5e
tofu345 <shammahayotunde1@gmail.com>
2026-03-14 11:50:27 +0000

    reflect change from moving dwmblock scripts to my dwm repo

--- blocks.def.h
+++ blocks.def.h
@@ -1,20 +1,15 @@
-// colours with status2d https://dwm.suckless.org/patches/status2d/
-
-#define SCRIPT(s) "~/dotfiles/bin/" s
-
-static char volume_cmd[]	= SCRIPT("dwmb_volume.sh");
-static char wifi_cmd[]		= SCRIPT("dwmb_wifi.sh");
-static char cpu_usage_cmd[] = SCRIPT("dwmb_cpu_usage.sh");
-static char cpu_load_cmd[]	= SCRIPT("dwmb_cpu_load.sh");
-static char memory_cmd[]	= SCRIPT("dwmb_memory.sh");
-static char battery_cmd[]	= SCRIPT("dwmb_battery.sh");
+static char volume_cmd[]	= "dwmb_volume.sh";
+static char wifi_cmd[]		= "dwmb_wifi.sh";
+static char cpu_usage_cmd[] = "dwmb_cpu_usage.sh";
+static char cpu_load_cmd[]	= "dwmb_cpu_load.sh";
+static char memory_cmd[]	= "dwmb_memory.sh";
+static char battery_cmd[]	= "dwmb_battery.sh";
 static char date_cmd[]		= "date '+%a %b %d %H:%M '";
 
-// Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /* Icon		Command			Update (s)		Update Signal */
     { "",       volume_cmd,     1,		        1 },
-    { "",       wifi_cmd,       1,		        0 },
+    { "",       wifi_cmd,       5,		        0 },
     { "",       cpu_usage_cmd,  1,		        0 },
     { "",       cpu_load_cmd,   1,		        0 },
     { "",       memory_cmd,     1, 		        0 },
