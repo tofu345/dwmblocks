ba519eaef327990604f6299ec131c137119aa42d
tofu345 <shammahayotunde1@gmail.com>
2026-08-13 18:52:08 +0100

    temperature block

--- blocks.def.h
+++ blocks.def.h
@@ -2,6 +2,7 @@ static const Block blocks[] = {
     /* icon     command                     update (s)  update signal */
     { " ",      "dwmblocks-vol",            5,          1 },
     { "",       "dwmblocks-cpu",            1,          0 },
+    { "",       "dwmblocks-temp",           1,          0 },
     { "",       "dwmblocks-mem",            1,          0 },
     { "",       "dwmblocks-bat",            300,        2 },
     { "",       "date '+%a %b %d %H:%M '",  1,          0 },
